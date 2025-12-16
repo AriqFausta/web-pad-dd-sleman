93832efc9e4d800045943d7097e8a98ceaa9aed3
AriqFausta <ariqfausta@gmail.com>
2025-12-17 00:35:03 +0700

    Update kategoris table migration for column names

--- database/migrations/2025_10_24_100100_create_kategoris_table.php
+++ database/migrations/2025_10_24_100100_create_kategoris_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('kategoris', function (Blueprint $table) {
-            $table->id('kategori_id');
-            $table->string('nama_kategori');
+            $table->id(); // PENTING
+            $table->string('nama');
             $table->timestamps();
         });
     }
