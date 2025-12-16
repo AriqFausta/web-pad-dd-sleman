d34cdf596afd038e5a092138c81da3dda987681f
AriqFausta <ariqfausta@gmail.com>
2025-12-17 02:13:42 +0700

    Update DatabaseSeeder to include additional seeders
    Added calls to new seeders for categories, content, and organizations.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\konten;
+use App\Models\kategori;
+use App\Models\Organisasi_Card;
+use App\Models\Organisasi;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,6 +20,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+            $this->call([
+            AdminSeeder::class,
+            KategoriSeeder::class,
+            KontenSeeder::class,
+            OrganisasiCardSeeder::class,
+            OrganisasiSeeder::class,
+            
+        ]);
 
         User::firstOrCreate(
             ['email' => 'test@example.com'],
